fc2d3b17e7cef1565a8c79f30eb16bed3f9938e2
Leonhard Gahr <leonhard.gahr@gmail.com>
2020-03-25 12:18:52 +0100

    GB-83: Optimized GitLab Pages Job

---
+++ gitlab-pages/generate-dir-index.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+OUTPUT="index.html"
+echo "<html><head><title>Documentation</title><link rel='stylesheet' href='style.css'></head><body>" > $OUTPUT
+
+echo "<div class='box'><ul class='directory-list'>" >> $OUTPUT
+
+for filepath in $(find . -maxdepth 1 -mindepth 1 -type f -name "*.pdf" | sort); do
+  echo "  <li><a href=\"$filepath/\">$filepath</a></li>" >> $OUTPUT
+done
+
+for filepath in $(find . -maxdepth 1 -mindepth 1 -type d| sort); do
+  path=`basename "$filepath"`
+  echo " <li class='folder'>$path<ul>" >> $OUTPUT
+
+  for i in $(find "$filepath" -maxdepth 1 -mindepth 1 -type f -name "*.pdf"| sort); do
+    file=`basename "$i"`
+    echo "  <li><a href=\"/$path/$file\">$file</a></li>" >> $OUTPUT
+  done
+
+  echo "</ul></li>" >> $OUTPUT
+done
+echo "</ul></body></html>" >> $OUTPUT
