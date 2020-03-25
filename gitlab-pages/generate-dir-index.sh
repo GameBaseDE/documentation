92cca93731522f9d93e2bd5542b36abafc2bcc81
Leonhard Gahr <leonhard.gahr@gmail.com>
2020-03-25 11:28:44 +0100

    Update generate-dir-index.sh

--- gitlab-pages/generate-dir-index.sh
+++ gitlab-pages/generate-dir-index.sh
@@ -3,8 +3,21 @@
 OUTPUT="index.html"
 echo "<html><head><title>Documentation</title><link rel='stylesheet' href='style.css'></head><body>" > $OUTPUT
 
-echo "<ul>" >> $OUTPUT
-for filepath in $(find . -maxdepth 3 -mindepth 1| sort); do
+echo "<div class='box'><ul class='directory-list'>" >> $OUTPUT
+
+for filepath in $(find . -maxdepth 1 -mindepth 1 -type f| sort); do
   echo "  <li><a href=\"$filepath/\">$filepath</a></li>" >> $OUTPUT
+done
+
+for filepath in $(find . -maxdepth 1 -mindepth 1 -type d| sort); do
+  path=`basename "$filepath"`
+  echo " <li class='folder'>$path<ul>" >> $OUTPUT
+
+  for i in $(find "$filepath" -maxdepth 1 -mindepth 1 -type f| sort); do
+    file=`basename "$i"`
+    echo "  <li><a href=\"/$path/$file\">$file</a></li>" >> $OUTPUT
+  done
+
+  echo "</ul></li>" >> $OUTPUT
 done
 echo "</ul></body></html>" >> $OUTPUT
